900c3375edb5e0b0bad9e22a68e7118830ccd837
unknown <dainnaxjakarta91@gmail.com>
2023-05-24 16:26:55 +0700

    dfas

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -2,13 +2,11 @@ const express = require("express");
 const indexRoute = express.Router();
 
 indexRoute.get("/", (req, res) => {
-  res.send(`
-    <div>
-        <h2>Nama   : Muhammad Dani Haikal</h2>
-        <h2>Tim    : BE-8</h2>
-        <h2>Mentor : Luthfi Fitra Musyaffa</h2>
-    </div>
-    `);
+  res.json({
+    nama: "Muhammad Dani Haikal",
+    tim: "BE-8",
+    mentor: "Luthfi Fitra Musyaffa",
+  });
 });
 
 module.exports = indexRoute;
